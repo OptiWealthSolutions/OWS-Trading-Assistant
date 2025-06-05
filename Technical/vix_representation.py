b8893d8756543c92cdc1ee13cdbca13f8961fa7b
optiwealth_solutions <74813101+Laiho13@users.noreply.github.com>
2025-06-05 16:08:05 +0200

    creation fonctoin get_vix_data

--- Technical/vix_representation.py
+++ Technical/vix_representation.py
@@ -1,2 +1,29 @@
-import sys
-print(sys.executable)
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_vix_data(ticker="^VIX"):
+    data = yf.download(ticker, period='1mo', interval='1h')
+    data.columns = data.columns.droplevel(1)
+    print(data)
+    last_close = data['Close'].iloc[-1]
+
+    if last_close >= 20:
+        print("⚠️  High volatility detected")
+    else:
+        print("✅  Normal volatility level")
+
+
+def get_vix_signal(ticker="^VIX"):
+    data = yf.download(ticker, period='1mo', interval='1h')
+    data.columns = data.columns.droplevel(1)
+    last_close = data['Close'].iloc[-1]
+
+    if last_close > 20:
+        return "SELL or HEDGE (high vol)"
+    else:
+        return "BUY or HOLD (normal vol)"
+get_vix_data()
+
+get_vix_signal()
