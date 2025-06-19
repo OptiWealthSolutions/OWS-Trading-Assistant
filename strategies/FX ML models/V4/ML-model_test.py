45b93e1724160210c41f28ae2ad382e22246864d
optiwealth_solutions <74813101+Laiho13@users.noreply.github.com>
2025-06-19 10:59:23 +0200

    maj

--- strategies/FX ML models/V4/ML-model_test.py
+++ strategies/FX ML models/V4/ML-model_test.py
@@ -1,12 +1,31 @@
+from ta.momentum import RSIIndicator, ROCIndicator, StochasticOscillator
+from ta.trend import ADXIndicator, SMAIndicator, EMAIndicator, MACD
+from ta.volatility import BollingerBands, AverageTrueRange
 import pandas as pd
-from ta.trend import SMAIndicator, EMAIndicator, MACD
-from ta.volatility import BollingerBands
-from ta.momentum import RSIIndicator, ROCIndicator
-from ta.volatility import AverageTrueRange
-from fx_strategy_V4 import get_interest_rate_difference
-from sklearn import *
+import numpy as np
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import yfinance as yf
+from fx_strategy_V4 import *
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, TimeSeriesSplit
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+import settings
+from xgboost import XGBClassifier
+from sklearn.metrics import ConfusionMatrixDisplay
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import TimeSeriesSplit
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
+import matplotlib.font_manager as fm
 import warnings
-warnings.filterwarnings("ignore")
 
 
 
@@ -73,4 +92,27 @@ def prepare_dataset_signal(spread, zscore, pair1_close, gold_price, adx, macro_d
     X = df[feature_cols]
     y = df['target']
 
-    return X, y
+    return X,y
+
+def ml_model(X,y):
+    """
+    Entraîne un modèle de régression logistique multinomiale sur les données fournies.
+    """
+    # Séparation des données en ensembles d'entraînement et de test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+    # Création du modèle de régression logistique multinomiale
+    model = RandomForestClassifier(nb_estimators=100,n_jobs=-1, random_state=42)
+
+    # Entraînement du modèle
+    model.fit(X_train, y_train)
+
+    # Prédictions sur l'ensemble de test
+    y_pred = model.predict(X_test)
+
+    # Évaluation du modèle
+    from sklearn.metrics import classification_report, confusion_matrix
+    print(classification_report(y_test, y_pred))
+    print(confusion_matrix(y_test, y_pred))
+
+    return model
---
+++ strategies/FX ML models/V4/settings.py
@@ -0,0 +1,41 @@
+# settings.py
+
+tickers = [
+    "EURUSD=X",
+    "GBPUSD=X",
+    "USDJPY=X",
+    "USDCAD=X",
+    "USDCHF=X",
+    "NZDUSD=X",
+    "EURJPY=X",
+    "EURGBP=X",
+    "EURCHF=X",
+    "GBPJPY=X",
+    "AUDJPY=X",
+]
+
+correlation_matrix = {
+    ("EURUSD=X", "GBPUSD=X"): 0.85,
+    ("EURUSD=X", "USDJPY=X"): -0.40,
+    ("GBPUSD=X", "USDJPY=X"): -0.35,
+    ("AUDUSD=X", "NZDUSD=X"): 0.90,
+    ("EURUSD=X", "EURJPY=X"): 0.75,
+    ("USDCHF=X", "USDJPY=X"): 0.80,
+    ("EURUSD=X", "USDCAD=X"): -0.50,
+    ("GBPUSD=X", "EURGBP=X"): -0.88,
+    ("USDCHF=X", "EURCHF=X"): 0.95,
+    # Complète selon tes analyses
+}
+
+commodity_mapping = {
+    "USD": "CL=F",   # Crude Oil
+    "EUR": "GC=F",   # Gold
+    "GBP": "GC=F",
+    "JPY": "SI=F",   # Silver
+    "AUD": "GC=F",  # Gold spot
+    "CAD": "CL=F",
+    "CHF": "XAG=X",  # Silver spot
+    "NZD": "GC=F",
+    "MXN": "CL=F",   # Par défaut pétrole
+    "ZAR": "GC=F"   # Par défaut or
+}
